815ff9a368d8a5ba0cc50064462ecf9c5c9e059c
PimPlaying <pimbakker2012@gmail.com>
2026-09-25 20:51:54 +0200

    💡Driver

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\rekka\Documents\vptfstrack\.venv"
+set "VIRTUAL_ENV=E:\vptfstrack\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
